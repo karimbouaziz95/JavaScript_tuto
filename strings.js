f077034fd82daaf57cc1a1b3d38f415139e84013
Karim Bouaziz <karimbouazizpip@gmail.com>
2021-12-25 04:02:21 +0100

    strings

---
+++ strings.js
@@ -0,0 +1,28 @@
+"use strict"
+
+let greeting = "  ---Karim--- "
+
+console.log(greeting.length)
+
+console.log(greeting.charAt(3))
+
+console.log(greeting[3])
+
+console.log(greeting.indexOf("i"))
+console.log(greeting.indexOf("---", 5))
+
+console.log(greeting.toUpperCase())
+console.log(greeting.toLowerCase())
+
+console.log(greeting.replace("---", "***"))
+
+console.log(greeting.trim())
+
+console.log("Hallo\nWelt")
+console.log("Hallo\\\nWelt")
+
+console.log("-".repeat(25))
+
+console.log("Hallo Welt".slice((6,8)))
+
+console.log("Hallo Welt".substr(6,4))
